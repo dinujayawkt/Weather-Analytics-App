cde054d546ba002cc9d9902dfdafd1861e609c03
ThamojDinujaya <w.k.thamoj@gmail.com>
2026-03-11 23:57:04 +0530

    Created basic setup for frontend and backend

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+     <div>
+        <h1>Welcome to the App</h1>
+
+
+     </div>
+    </>
+  )
+}
+
+export default App
